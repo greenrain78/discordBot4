ca22167fe4687d31d56a155fba4aab779c29571e
daewon kim <kimdw010130@daum.net>
2021-03-29 13:54:59 +0900

    edit basic state and version up 4.0.6

--- SubService/Basic/basic_engine.py
+++ SubService/Basic/basic_engine.py
@@ -20,10 +20,13 @@ class BasicEngine:
         runningTime = datetime.now() - cls.startTime
 
         title = "포인트 봇 정보"
-        text = f"포인트 봇 정보\n" \
-               f"실행 시간: {cls.startTime}\n" \
-               f"동작 시간: {runningTime.days}일, {runningTime.seconds}초\n" \
-               f"잠수 리스트: {sleepList}\n"
-
+        text = f"실행 시간: {cls.startTime}\n" \
+               f"동작 시간: {runningTime.days}일, {runningTime.seconds}초\n"
         em = Embed(title=title, description=text)
+
+        title1 = "잠수 리스트"
+        text1 = ""
+        for user, value in sleepList.items():
+            text1 += f"{user}: {value}일째 잠수중\n"
+        em.add_field(name=title1, value=text1, inline=False)
         return em
