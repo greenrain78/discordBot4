79f5434c7fa6ff0de43e73232526016fa030315f
daewon kim <kimdw010130@daum.net>
2021-04-04 17:06:22 +0900

    create report_issue

--- SubService/Basic/basic_bot.py
+++ SubService/Basic/basic_bot.py
@@ -12,19 +12,7 @@ class BasicBot(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.group()
-    async def basic(self, ctx):
-        """
-        basic 명령어
-        디코 봇에 대한 기본 명령어
-        """
-        if ctx.invoked_subcommand is None:
-            text = f"해당 명령어가 없습니다.\n" \
-                   f"명령어를 제대로 입력해 주세요.\n" \
-                   f"잘 모르시면 $help basic"
-            await ctx.send(text)
-
-    @basic.command()
+    @commands.command()
     async def state(self, ctx):
         """
         디코 봇 상태 확인
@@ -36,8 +24,7 @@ class BasicBot(commands.Cog):
         await ctx.message.delete()  # 입력된 명령 제거
         await msg.delete()  # 메세지 삭제
 
-
-    @basic.command()
+    @commands.command()
     async def github(self, ctx):
         """
         디코 봇 깃허브 링크
@@ -49,3 +36,14 @@ class BasicBot(commands.Cog):
         await ctx.message.delete()  # 입력된 명령 제거
         await msg.delete()  # 메세지 삭제
 
+    @commands.command()
+    async def report(self, ctx):
+        """
+        정식적으로 디코봇 버그 리포트 제출
+        """
+        embed = BasicEngine.report_issue()
+        msg = await ctx.send(embed=embed)
+        await asyncio.sleep(60)
+
+        await ctx.message.delete()  # 입력된 명령 제거
+        await msg.delete()  # 메세지 삭제
--- SubService/Basic/basic_engine.py
+++ SubService/Basic/basic_engine.py
@@ -31,3 +31,13 @@ class BasicEngine:
             text1 += f"{user}: {PointDB.get_point(user)}pt - {value}일째 잠수\n"
         em.add_field(name=title1, value=text1, inline=False)
         return em
+
+    @classmethod
+    def report_issue(cls) -> Embed:
+        title = "버그 제보"
+        text = f"아래 링크를 눌러 개발자에게 직접 버그를 제보하세요.\n" \
+               f"그럼 언젠가 반영이 될지도?\n" \
+               f"https://github.com/greenrain78/discordBot4/issues/new"
+        em = Embed(title=title, description=text)
+
+        return em
